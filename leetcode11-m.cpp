c9c233c6f7fe572cc35a6c1e594698bf6009a1a9
XiaoQY123 <pan906601155@hotmail.com>
2022-08-31 12:58:20 +0800

    11.盛最多水的容器

---
+++ leetcode11-m.cpp
@@ -0,0 +1,34 @@
+﻿#include<stdio.h>
+#include<map>
+#include<string>
+#include <unordered_map>
+using namespace std;
+int main() {
+	class Solution {
+	public:
+		int maxArea(vector<int>& height) {
+			
+			int i = 0;
+			int j = height.size() - 1;
+			int maxnum = min(height[i], height[j]) * (j - i);
+			
+			for (; i<j; )
+			{
+				
+				if (height[i] < height[j])
+					i++;
+				else
+					j--;
+				maxnum = max(maxnum, min(height[i], height[j]) * (j - i));
+
+			}
+			return maxnum;
+		}
+	};
+
+	Solution a;
+	vector<int> height = { 1,8,6,2,5,4,8,3,7 };
+	printf("%d",a.maxArea(height));
+	
+	
+}
