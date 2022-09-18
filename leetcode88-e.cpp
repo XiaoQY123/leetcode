4d1555ae2d2f31c48fa9430272b2b74fd0a9ef8a
XiaoQY123 <pan906601155@hotmail.com>
2022-09-18 23:17:11 +0800

    88.合并有序数列

---
+++ leetcode88-e.cpp
@@ -0,0 +1,115 @@
+﻿#include<stdio.h>
+#include<map>
+#include<string>
+#include <unordered_map>
+#include<iostream>
+#include<algorithm>
+#include <stack>
+#include<queue>
+using namespace std;
+
+/*给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+struct Status {
+    int val;
+    ListNode* ptr;
+    bool operator < (const Status& rhs) const {
+        return val > rhs.val;
+    }
+};
+
+int main() {
+    class Solution {
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            int i = m-1;
+            int j = n-1;
+            int count = 0;
+            int v;
+            if (m == 0)
+                nums1[0] = INT_MIN;//边界问题
+            for (;j>=0; count++) {
+                v= n + m - 1 - count;
+                if (i <= -1)//边界问题
+                    i = 0;
+                if (j <=-1)
+                    j = 0;
+                if (v < 0)
+                    v = 0;
+                if (nums1[i] <nums2[j]) {
+                    nums1[v] = nums2[j];
+                    j--;
+                }
+                else {
+                    nums1[v] = nums1[i];
+                    nums1[i] = INT_MIN;
+                    printf("%d, %d\n", count,nums1[v]);
+                    i--;
+                }
+            }
+        }
+    };
+
+    vector<int> nums1 = { 0,0,0,0,0,0};
+    vector<int> nums2 = { -1,-1,1,1,2,3};
+    Solution a;
+    a.merge(nums1,0, nums2,6);
+    for (int i = 0; i < nums1.size();i++)
+    printf("%d ", nums1[i]);
+}
+
+   /*  ListNode* head = new ListNode(1);
+     ListNode* h = new ListNode(2);
+     head->next = h;
+     ListNode* hh = new ListNode(3);
+     h->next = hh;*/
+
+
+    //ListNode* head2 = new ListNode(1);
+    //ListNode* h2 = new ListNode(3);
+    //head2->next = h2;
+    //ListNode* hh2 = new ListNode(4);
+    //h2->next = hh2;
+
+    //ListNode* head3 = new ListNode(2);
+    //ListNode* h3 = new ListNode(6);
+    //head3->next = h3;
+    // ListNode* hh3 = new ListNode(17);
+    // h3->next = hh3;
+
+   /* vector<ListNode*> lists;
+    lists.push_back(head);*/
+    //lists.push_back(head2);
+    //lists.push_back(head3);
+
+    //Solution a;
+    //ListNode* ret = a.rotateRight(head,1);
+    //ListNode* p = head;
+    //int c = 0;
+    //for (; p != nullptr;) {
+    //    printf("%d  ", p->val);
+    //    // c++;
+    //    p = p->next;
+    //    //printf("%d ..", c);
+    //}
+//}
+
+
+
+
+
