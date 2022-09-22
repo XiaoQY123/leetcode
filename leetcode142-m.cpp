03a04c8a9326b9d7a8e3ffb3f1cf845436b7689d
XiaoQY123 <pan906601155@hotmail.com>
2022-09-22 21:17:41 +0800

    142.环形链表II

---
+++ leetcode142-m.cpp
@@ -0,0 +1,116 @@
+﻿#include<stdio.h>
+#include<map>
+#include<string>
+#include <unordered_map>
+#include<iostream>
+#include<algorithm>
+#include <stack>
+#include<queue>
+#include <bitset>
+#include <unordered_set>
+using namespace std;
+
+/*给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/single-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+struct Status {
+    int val;
+    ListNode* ptr;
+    bool operator < (const Status& rhs) const {
+        return val > rhs.val;
+    }
+};
+
+
+
+
+  struct TreeNode {
+      int val;
+     TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+  
+int main() {
+    class Solution {
+    public:
+        ListNode* detectCycle(ListNode* head) {
+            unordered_set<ListNode*> seen;
+            while (head != nullptr) {
+                if (seen.count(head)) {
+                    return head;
+                }
+                seen.insert(head);
+                head = head->next;
+            }
+            return nullptr;
+        }
+    };
+
+       ListNode* head = new ListNode(1);
+       ListNode* h = new ListNode(2);
+       head->next = h;
+       ListNode* hh = new ListNode(3);
+       h->next = hh;
+
+       Solution a;
+       ListNode* cc= a.detectCycle(head);
+      
+}
+
+   /*  ListNode* head = new ListNode(1);
+     ListNode* h = new ListNode(2);
+     head->next = h;
+     ListNode* hh = new ListNode(3);
+     h->next = hh;*/
+
+
+    //ListNode* head2 = new ListNode(1);
+    //ListNode* h2 = new ListNode(3);
+    //head2->next = h2;
+    //ListNode* hh2 = new ListNode(4);
+    //h2->next = hh2;
+
+    //ListNode* head3 = new ListNode(2);
+    //ListNode* h3 = new ListNode(6);
+    //head3->next = h3;
+    // ListNode* hh3 = new ListNode(17);
+    // h3->next = hh3;
+
+   /* vector<ListNode*> lists;
+    lists.push_back(head);*/
+    //lists.push_back(head2);
+    //lists.push_back(head3);
+
+    //Solution a;
+    //ListNode* ret = a.rotateRight(head,1);
+    //ListNode* p = head;
+    //int c = 0;
+    //for (; p != nullptr;) {
+    //    printf("%d  ", p->val);
+    //    // c++;
+    //    p = p->next;
+    //    //printf("%d ..", c);
+    //}
+//}
+
+
+
+
+
